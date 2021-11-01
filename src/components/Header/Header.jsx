f2deebd69758d2996a7c3bfc644316832d1f1e2c
karmovanzor <Anzor.Karmov@nordigy.ru>
2021-11-01 17:57:17 +0300

    markup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import css from "./Header.module.css"
+
+const Header = () => {
+    return (
+        <div className={css.header}><h1>Nofapp</h1></div>
+    )
+}
+
+export default Header
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import css from "./Navbar.module.css"
+
+const Navbar = () => {
+    return (
+        <div className={css.navbar}>Navbar</div>
+    )
+}
+
+export default Navbar
