6949385ed32403a502d76de1566adcc120fb7312
zor07 <anzor.karmov@gmail.com>
2022-01-08 01:36:11 +0300

    login, me and refresh token

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,33 @@
 import axios from "axios";
+import Cookies from "universal-cookie";
 
 const instance = axios.create({
     baseURL: 'http://localhost:8888/api/v1/'
 })
 
+const cookies = new Cookies()
 
 export const AUTH_API = {
     login(username, password) {
         return instance.post(`auth/login`, {username, password})
-            .then(response => response.data)
     },
-    refreshAccessToken(refreshToken) {
+    me() {
+        return instance.get(`auth/me`, {
+            headers : {
+                "Authorization": `Bearer ${cookies.get("accessToken")}`
+            }
+        }).catch((error) => {
+            if (error.response && error.response.status === 403) {
+                return error.response
+            } else {
+                console.error(error)
+            }
+        })
+    },
+    refreshAccessToken() {
         return instance.get(`auth/token/refresh`, {
             headers : {
-                "Authorization": `Bearer ${refreshToken}`
+                "Authorization": `Bearer ${cookies.get("refreshToken")}`
             }
         })
     }
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,34 +1,64 @@
 import {AUTH_API} from "../api/api";
+import Cookies from 'universal-cookie';
 
-const SET_ACCESS_TOKEN = "AUTH/SET_ACCESS_TOKEN"
-const SET_REFRESH_TOKEN = "AUTH/SET_REFRESH_TOKEN"
+const SET_USER_DATA = "AUTH/SET_USER_DATA"
 
 const initialState = {
-    accessToken: null,
-    refreshToken: null
+    id: null,
+    name: null,
+    username: null
 }
 
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
-        case SET_ACCESS_TOKEN:
-            return {...state, accessToken: action.accessToken}
-        case SET_REFRESH_TOKEN:
-            return {...state, refreshToken: action.refreshToken}
+        case SET_USER_DATA:
+            return {...state, id: action.id, name: action.name, username: action.username}
         default:
             return state;
     }
 }
 
-const setAccessToken = (accessToken) => ({type: SET_ACCESS_TOKEN, accessToken})
-const setRefreshToken = (refreshToken) => ({type: SET_ACCESS_TOKEN, refreshToken})
+const setUserData = (id, name, username) => ({type: SET_USER_DATA, id, name, username})
+
+export const me = () => {
+    return async (dispatch) => {
+        const response = await AUTH_API.me()
+        if (response.status === 200) {
+            dispatch(setUserData(response.id, response.name, response.username))
+        } else if (response.status === 403 && response.data.error_message.includes('The Token has expired')) {
+            dispatch(refreshToken())
+        }
+
+    }
+}
 
 export const login = (username, password) => {
     return async (dispatch) => {
         const response = await AUTH_API.login(username, password);
-        dispatch(setAccessToken(response.access_token))
-        dispatch(setRefreshToken(response.refresh_token))
+        if (response.status === 200) {
+            updateCoockies(response.data.access_token, response.data.refresh_token)
+            setTimeout(() => {
+                dispatch(me())
+            }, 3000)
+        }
+
     }
 }
 
+export const refreshToken = () => {
+    return async (dispatch) => {
+        const response = await AUTH_API.refreshAccessToken()
+        if (response.status === 200) {
+            updateCoockies(response.data.access_token, response.data.refresh_token)
+            dispatch(me())
+        }
+    }
+}
+
+const updateCoockies = (accessToken, refreshToken) => {
+    const cookies = new Cookies();
+    cookies.set('accessToken', accessToken, {path: '/'});
+    cookies.set('refreshToken', refreshToken, {path: '/'});
+}
 
 export default authReducer;
