2cf853fe7d378c7371dc0d1c91a1c0de228fd133
zor07 <anzor.karmov@gmail.com>
2022-01-29 23:14:27 +0300

    undo redo svg

--- src/components/Editor/Toolbar/Buttons/Redo.tsx
+++ src/components/Editor/Toolbar/Buttons/Redo.tsx
@@ -7,8 +7,21 @@ const Redo = () => {
         actions.redo()
     };
 
-    return <button
-        className="remirror-role remirror-button remirror-tabbable"
-        onClick={redo}>Redo</button>;
+    return (
+        <button className="remirror-role remirror-button remirror-tabbable"
+                onClick={redo}>
+            <svg stroke="currentColor"
+                 fill="currentColor"
+                 stroke-width="0"
+                 name="arrowGoForwardFill"
+                 height="1em"
+                 width="1em"
+                 xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 24 24">
+                <path fill="none" d="M0 0h24v24H0z"/>
+                <path d="M16 7h-5a6 6 0 1 0 0 12h9v2h-9a8 8 0 1 1 0-16h5V1l6 5-6 5V7z"/>
+            </svg>
+        </button>
+    )
 }
 export default Redo
--- src/components/Editor/Toolbar/Buttons/Undo.tsx
+++ src/components/Editor/Toolbar/Buttons/Undo.tsx
@@ -7,9 +7,22 @@ const Undo = () => {
         actions.undo()
     };
 
-    return <button
-        className="remirror-role remirror-button remirror-tabbable"
-        onClick={undo}>Undo</button>;
+    return (
+        <button className="remirror-role remirror-button remirror-tabbable"
+                onClick={undo}>
+            <svg stroke="currentColor"
+                 fill="currentColor"
+                 stroke-width="0"
+                 name="arrowGoBackFill"
+                 height="1em"
+                 width="1em"
+                 xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 24 24">
+                <path fill="none" d="M0 0h24v24H0z"/>
+                <path d="M8 7v4L2 6l6-5v4h5a8 8 0 1 1 0 16H4v-2h9a6 6 0 1 0 0-12H8z"/>
+            </svg>
+        </button>
+    )
 }
 
 export default Undo
