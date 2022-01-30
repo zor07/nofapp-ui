f2d7ebacaaf4b86d9f5e1b5abd263813a92862df
zor07 <anzor.karmov@gmail.com>
2022-01-30 21:56:21 +0300

    bold icon

--- src/components/Editor/Toolbar/Buttons/Bold.tsx
+++ src/components/Editor/Toolbar/Buttons/Bold.tsx
@@ -1,19 +1,20 @@
 import {useActive, useCommands} from "@remirror/react";
 import React from "react";
+import {cx} from "remirror";
+import {BoldIcon} from "./Icons";
 
 const Bold = () => {
     const { toggleBold, focus } = useCommands();
     const active = useActive();
 
     return (
-        <button
-            className="remirror-role remirror-button remirror-tabbable"
-            onClick={() => {
-                toggleBold();
-                focus();
-            }}
-            style={{ fontWeight: active.bold() ? 'bold' : undefined }}>
-            B
+        <button className={cx(active.bold() ? 'remirror-button-active' : '', 'remirror-role remirror-button remirror-tabbable') }
+                onClick={() => {
+                    toggleBold();
+                    focus();
+                }}
+                style={{ fontWeight: active.bold() ? 'bold' : undefined }}>
+            <BoldIcon />
         </button>
     );
 }
