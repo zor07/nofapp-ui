ca56b723d4c866a5315b5a0f4d6f7b3ed290795a
zor07 <anzor.karmov@gmail.com>
2022-02-05 19:59:24 +0300

    Diary List antd

--- src/components/Diary/DiaryListContainer.tsx
+++ src/components/Diary/DiaryListContainer.tsx
@@ -3,9 +3,10 @@ import {connect, useDispatch} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
 import {deleteDiary, DiaryIdAndTitleType, requestDiaries} from "../../redux/diaries-reducer";
 import {NavLink} from "react-router-dom";
-import css from './Diary.module.css'
 import {compose} from "redux";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {Button, List} from "antd";
+import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
 
 type MapStatePropsType = {
     diaries: Array<DiaryIdAndTitleType>
@@ -37,31 +38,40 @@ const DiaryListContainer: React.FC<DiariesContainerPropsType> = (props) => {
         }
     }, [deleteDiaryId])
 
-    const notes = props.diaries.map(diary =>
-        <li key={diary.id}>
-            <NavLink to={`/diary/editor/${diary.id}`}>
-                {diary.title}
-            </NavLink>
-            <div>
-                <button onClick={() => setDeleteDiaryId(diary.id)}> Delete </button>
-            </div>
-
-        </li>);
-
     return (
-        <div className={css.diaryList}>
+        <div>
             <h2>Diary</h2>
-            <ul>
-                {notes}
-            </ul>
+            <List itemLayout="horizontal"
+                  size="small"
+                  pagination={{
+                      onChange: page => {
+                          console.log(page);
+                      },
+                      pageSize: 10,
+                  }}
+                  dataSource={props.diaries}
+                  renderItem={item => (
+                      <List.Item key={item.id}
+                                 actions={[
+                                     <NavLink to={`/diary/editor/${item.id}`}>
+                                         <Button icon={<EditOutlined/>}/>
+                                     </NavLink>,
+                                     <Button danger icon={<DeleteOutlined/>} onClick={() => setDeleteDiaryId(item.id)}/>
+                                 ]}>
+                          {item.title}
+                      </List.Item>
+                  )}/>
             <div>
                 <NavLink to={`/diary/editor`}>
-                    New note
+                    <Button type="primary">
+                        New note
+                    </Button>
                 </NavLink>
             </div>
         </div>
     )
 }
+
 
 let mapStateToProps = (state: AppStateType): MapStatePropsType => {
     return {
