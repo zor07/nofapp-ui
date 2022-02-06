a2897e9d752a846f4e88604d36f99b0ed3039e0f
zor07 <anzor.karmov@gmail.com>
2022-02-07 00:06:03 +0300

    date utils ts, currentDateString method

--- src/utils/dateUtils.js
+++
@@ -1,8 +0,0 @@
-export const getCurrentDate = () => {
-    return adjustForTimezone(new Date().toISOString());
-}
-
-export const adjustForTimezone = (dateString) => {
-    const date = new Date(dateString)
-    return new Date(date.setMinutes(date.getMinutes() - date.getTimezoneOffset())).toISOString();
-}
