57609112285b0002b37845401baacc1ac17eb894
zor07 <anzor.karmov@gmail.com>
2022-02-12 18:21:04 +0300

    add video button works with custom videos

--- src/components/Editor/Toolbar/Buttons/AddYoutubeButton.tsx
+++ src/components/Editor/Toolbar/Buttons/AddYoutubeButton.tsx
@@ -1,15 +1,46 @@
 import {useCommands} from "@remirror/react";
-import React from "react";
+import React, {useState} from "react";
+import {Input, Modal} from "antd";
 
 const AddYoutubeButton = () => {
+    const [visible, setVisible] = React.useState(false);
+    const [youTubeVideoLink, setYouTubeVideoLink] = useState('')
+
     const commands = useCommands();
-    const handleClick = () => commands.addYouTubeVideo({ video: 'Zi7sRMcJT-o', startAt: 450 });
+
+    const showModal = () => {
+        setVisible(true);
+    };
+
+    const handleOk = () => {
+        commands.addYouTubeVideo({ video: youTubeVideoLink})
+        setVisible(false);
+        setYouTubeVideoLink('')
+    };
+
+    const handleCancel = () => {
+        console.log('Clicked cancel button');
+        setVisible(false);
+    };
+
     return (
-        <button className="remirror-role remirror-button remirror-tabbable"
-                onMouseDown={(event) => event.preventDefault()}
-                onClick={handleClick}>
-            Add video
-        </button>
+        <>
+            <button className="remirror-role remirror-button remirror-tabbable"
+                    onMouseDown={(event) => event.preventDefault()}
+                    onClick={showModal}>
+                Add video
+            </button>
+            <Modal
+                title="Insert YouTube video link"
+                visible={visible}
+                onOk={handleOk}
+                onCancel={handleCancel}>
+                <Input placeholder="https://www.youtube.com/"
+                       value={youTubeVideoLink}
+                       onChange={(e) => setYouTubeVideoLink(e.target.value)}/>
+            </Modal>
+        </>
+
     );
 };
 
