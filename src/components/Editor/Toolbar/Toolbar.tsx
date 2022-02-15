2ba42613fa1cadab83e1e32f5051da0f38ce9fac
zor07 <anzor.karmov@gmail.com>
2022-02-15 23:46:51 +0300

    Toolbar font size controls

--- src/components/Editor/Toolbar/Toolbar.tsx
+++ src/components/Editor/Toolbar/Toolbar.tsx
@@ -13,9 +13,10 @@ import Strike from "./Controls/Strike";
 import AlignButtons from "./Controls/AlignButtons";
 import AddYoutubeButton from "./Controls/AddYoutubeButton";
 import AddImageButton from "./Controls/AddImageButton";
-import FontSizeControl from "./Controls/FontSizeControl";
 import {cx} from "remirror";
 import css from './Controls/Controls.module.css'
+import IncreaseFontSizeButton from "./Controls/IncreaseFontSizeButton";
+import DecreaseFontSizeButton from "./Controls/DecreaseFontSizeButton";
 
 
 type ToolbarPropsType = {
@@ -37,6 +38,11 @@ const Toolbar: React.FC<ToolbarPropsType> = ({saveContent}) => {
             <hr role="separator" aria-orientation="vertical" className="remirror-role remirror-separator"/>
             <div className="remirror-role remirror-group">
                 <HeadingButtons />
+            </div>
+            <hr role="separator" aria-orientation="vertical" className="remirror-role remirror-separator"/>
+            <div className="remirror-role remirror-group">
+                <IncreaseFontSizeButton/>
+                <DecreaseFontSizeButton/>
             </div>
             <hr role="separator" aria-orientation="vertical" className="remirror-role remirror-separator"/>
             <div className="remirror-role remirror-group">
