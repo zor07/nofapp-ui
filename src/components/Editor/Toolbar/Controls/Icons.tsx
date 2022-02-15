4a9598d6de89d1b39ef8f5b948d7c752a0c67075
zor07 <anzor.karmov@gmail.com>
2022-02-15 23:04:26 +0300

    arrow down icon

--- src/components/Editor/Toolbar/Controls/Icons.tsx
+++ src/components/Editor/Toolbar/Controls/Icons.tsx
@@ -387,3 +387,19 @@ export const ArrowUpIcon = () => {
     )
 }
 
+export const ArrowDownIcon = () => {
+    return (
+        <svg stroke="currentColor"
+             fill="currentColor"
+             strokeWidth="0"
+             name="table2"
+             height="1em"
+             width="1em"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 24 24">
+            <path fill="none"
+                  d="M0 0h24v24H0z"/>
+            <path d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z"/>
+        </svg>
+    )
+}
