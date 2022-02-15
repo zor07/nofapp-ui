64b16c55270e006bf141be3124c53fb6aaae93c1
zor07 <anzor.karmov@gmail.com>
2022-02-15 20:25:50 +0300

    font size control wip

--- src/components/Editor/Toolbar/Controls/FontSizeControl.tsx
+++ src/components/Editor/Toolbar/Controls/FontSizeControl.tsx
@@ -1,11 +1,17 @@
 import {useActive, useCommands, useHelpers, useRemirrorContext} from "@remirror/react";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {cx} from "remirror";
 import {BoldIcon} from "./Icons";
 import {AutoComplete, Input } from "antd";
 import {UserOutlined} from "@ant-design/icons";
 import css from './Controls.module.css'
-import Select from "react-select";
+import Select, {ActionMeta, OnChangeValue} from "react-select";
+import CreatableSelect from "react-select/creatable";
+
+type FontSizeOption = {
+    value: string
+    label: string
+}
 
 const FontSizeControl = () => {
     const {setFontSize} = useCommands();
@@ -13,22 +19,43 @@ const FontSizeControl = () => {
     const {view} = useRemirrorContext({autoUpdate: true});
 
 
-    const [selectedOption, setSelectedOption] = useState({ value: '30', label: '30' });
+    const [selectedOption, setSelectedOption] = useState({ value: '31', label: '31' });
 
     const options = [
-        { value: '10', label: '10' },
+        { value: 10, label: '10' },
         { value: '20', label: '20' },
         { value: '30', label: '30' },
     ];
 
+    const handleChange = (
+        newValue: OnChangeValue<FontSizeOption, false>,
+        actionMeta: ActionMeta<FontSizeOption>
+    ) => {
+        console.group('Value Changed');
+        console.log(newValue);
+        console.log(`action: ${actionMeta.action}`);
+        console.groupEnd();
+    };
+    const handleInputChange = (inputValue: any, actionMeta: any) => {
+        console.group('Input Changed');
+        console.log(inputValue);
+        console.log(`action: ${actionMeta.action}`);
+        console.groupEnd();
+    };
+
     return (
         <>
             {/*"remirror-input"*/}
-            <Select className={css.select}
+            <CreatableSelect
+                    isClearable
+                    defaultMenuIsOpen
+                    className={css.select}
+                    // value={selectedOption}
                     filterOption={() => true}
-                    components={{ DropdownIndicator:() => null, IndicatorSeparator:() => null }}
+                    components={{ DropdownIndicator:() => null, ClearIndicator:() => null, IndicatorSeparator:() => null }}
                     options={options}
-                    placeholder="Font Size"/>
+                    onInputChange={handleInputChange.bind(this)}
+                    onChange={handleChange.bind(this)}/>
 
         </>
     );
