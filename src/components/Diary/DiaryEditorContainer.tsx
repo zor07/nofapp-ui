85aa21e331123ff3c44f41ee6a8c9661240230be
zor07 <anzor.karmov@gmail.com>
2022-02-18 09:37:58 +0300

    diary tabs

--- src/components/Diary/DiaryEditorContainer.tsx
+++ src/components/Diary/DiaryEditorContainer.tsx
@@ -2,15 +2,17 @@ import React, {useEffect, useState} from 'react';
 import {connect, useDispatch} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
 import {clearDiaryAction, DiaryType, getDiary, saveDiary} from "../../redux/diary-reducer";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import Editor from "../Editor/Editor";
 import {PrimitiveSelection, RemirrorJSON} from "remirror";
 import {Tabs} from "antd";
 import css from './Diary.module.css'
+import {DiaryIdAndTitleType} from "../../redux/diaries-reducer";
 
 
 type MapStatePropsType = {
-    diary: DiaryType | null
+    diary: DiaryType | null,
+    diaries: Array<DiaryIdAndTitleType>
 }
 
 type MapDispatchPropsType = {
@@ -24,8 +26,10 @@ type DiaryContainerPropsType = MapStatePropsType & MapDispatchPropsType
 const DiaryEditorContainer: React.FC<DiaryContainerPropsType> = (props) =>  {
 
     const params = useParams()
+    const navigate = useNavigate();
     const dispatch = useDispatch()
     const [diary, setDiary] = useState(props.diary)
+    const [currentDiaryId, setCurrentDiaryId] = useState(params.diaryId)
 
     useEffect(() => {
         if (params.diaryId) {
@@ -57,14 +61,28 @@ const DiaryEditorContainer: React.FC<DiaryContainerPropsType> = (props) =>  {
         }
     }
 
+    useEffect(() => {
+        if (currentDiaryId != params.diaryId) {
+            dispatch(getDiary(currentDiaryId));
+            navigate(`/diary/editor/${currentDiaryId}`)
+        }
+    }, [currentDiaryId])
+
+    const handleTabChange = (selectedDiaryId) => {
+        setCurrentDiaryId(selectedDiaryId)
+    }
+
     const { TabPane } = Tabs;
 
     return (
         <div>
-
-            <Tabs defaultActiveKey="1" tabPosition={'top'} style={{ height: "max-content"}}>
-                {[...Array.from({ length: 20 }, (v, i) => i)].map(i => (
-                    <TabPane tab={`Tab-${i}`} key={i} disabled={i === 28}>
+            <Tabs defaultActiveKey={params.diaryId}
+                  tabPosition={'right'}
+                  onChange={handleTabChange}
+                  style={{ height: "max-content"}}>
+                {props.diaries.map(diaryItem => (
+                    <TabPane tab={diaryItem.title}
+                             key={diaryItem.id} >
                         <div  className={css.editor}>
                             <Editor selection={props.diary.data.selection}
                                     content={props.diary.data.content}
@@ -81,7 +99,8 @@ const DiaryEditorContainer: React.FC<DiaryContainerPropsType> = (props) =>  {
 
 let mapStateToProps = (state: AppStateType): MapStatePropsType => {
     return {
-        diary: state.diary.diary
+        diary: state.diary.diary,
+        diaries: state.diaries.diaries
     }
 }
 
