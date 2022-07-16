463d9393c2b6b277ae320a88098f8469a37f82bc
zor07 <anzor.karmov@gmail.com>
2022-07-16 22:54:26 +0300

    add posts to state

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,3 +1,5 @@
+import {RemirrorJSON} from "remirror";
+
 export type ProfileType = {
     id: string | null
     avatarUri: string | null
@@ -7,6 +9,7 @@ export type ProfileType = {
 
 type InitialStateType =  {
     profile: ProfileType
+    posts: Array<RemirrorJSON>
 };
 
 type SetProfileActionType = {
@@ -14,6 +17,12 @@ type SetProfileActionType = {
     payload: ProfileType
 }
 
+type SetPostsActionType = {
+    type: typeof SET_POSTS
+    payload: Array<RemirrorJSON>
+}
+
+const SET_POSTS = 'PROFILE/SET_POSTS'
 const SET_PROFILE = 'PROFILE/SET_PROFILE'
 
 const initialState: InitialStateType = {
@@ -21,11 +30,31 @@ const initialState: InitialStateType = {
         id: "some id",
         userId: "user id",
         avatarUri: "avatar url",
-        timerStart: new Date("2021-11-08T01:00:00.000")
-    }
+        timerStart: new Date("2021-11-08T01:00:00.000"),
+    },
+    posts: [
+        {
+            type: "doc",
+            content: [
+                {
+                    type: "heading",
+                    attrs: {
+                        "level": 1
+                    },
+                    content: [
+                        {
+                            type: "text",
+                            text: "Some post"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 }
 
-const profileReducer = (state: InitialStateType = initialState, action: SetProfileActionType): InitialStateType => {
+const profileReducer = (state: InitialStateType = initialState,
+                        action: SetProfileActionType | SetPostsActionType): InitialStateType => {
     switch (action.type) {
         case "PROFILE/SET_PROFILE":
             return {
