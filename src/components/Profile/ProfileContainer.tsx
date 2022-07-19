72b898cfbac102e4fa2614c72b34f04b4f21e01b
zor07 <anzor.karmov@gmail.com>
2022-07-19 21:33:19 +0300

    getProfile in container

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,23 +1,37 @@
-import React from "react";
-import {ProfileType} from "../../redux/profile-reducer";
+import React, {useEffect} from "react";
+import {getProfile, ProfileType} from "../../redux/profile-reducer";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import Profile from "./Profile";
 import {compose} from "redux";
-import {connect} from "react-redux";
+import {connect, useDispatch} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
 import {RemirrorJSON} from "remirror";
+import {useParams} from "react-router-dom";
 
 type MapStatePropsType = {
+    currentUserId: string
     profile: ProfileType
     posts: Array<RemirrorJSON>
 }
 
 type MapDispatchPropsType = {
+    getProfile: (userId: string) => void
 }
 
 type ProfileContainerType = MapStatePropsType & MapDispatchPropsType
 
-const ProfileContainer: React.FC<ProfileContainerType> = ({profile, posts}) => {
+const ProfileContainer: React.FC<ProfileContainerType> = ({currentUserId, profile, posts}) => {
+    const dispatch = useDispatch()
+    const params = useParams()
+
+    useEffect(() => {
+        if (params.userId) {
+            dispatch(getProfile(params.userId))
+        } else {
+            dispatch(getProfile(currentUserId))
+        }
+    }, [])
+
     return (
         <Profile profile={profile} posts={posts}/>
     )
@@ -25,6 +39,7 @@ const ProfileContainer: React.FC<ProfileContainerType> = ({profile, posts}) => {
 
 let mapStateToProps = (state: AppStateType): MapStatePropsType => {
     return {
+        currentUserId: state.auth.id,
         profile: state.profile.profile,
         posts: state.profile.posts
     }
@@ -32,5 +47,5 @@ let mapStateToProps = (state: AppStateType): MapStatePropsType => {
 
 export default compose(
     withAuthRedirect,
-    connect<MapStatePropsType, MapDispatchPropsType, AppStateType>(mapStateToProps, {})
+    connect<MapStatePropsType, MapDispatchPropsType, AppStateType>(mapStateToProps, {getProfile})
 )(ProfileContainer);
