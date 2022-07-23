a2ce401a59f0441dd054014f3c9141e62f7de710
zor07 <anzor.karmov@gmail.com>
2022-07-23 22:17:41 +0300

    UserPosts component

---
+++ src/components/Profile/posts/UserPost.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import {Button, Divider} from "antd";
+import EditorReadView from "../../Editor/EditorReadView";
+import {RemirrorJSON} from "remirror";
+import {DeleteOutlined} from "@ant-design/icons";
+
+type MapStatePropsType = {
+    post: RemirrorJSON
+}
+
+type MapDispatchPropsType = {
+}
+
+
+const UserPost : React.FC<MapStatePropsType & MapDispatchPropsType> = ({post}) => {
+    return (
+        <div>
+            <Divider/>
+            <EditorReadView  data={post}
+                             displayTitle={true}/>
+            <Button danger icon={<DeleteOutlined/>}/>
+        </div>
+    )
+}
+
+export default UserPost
