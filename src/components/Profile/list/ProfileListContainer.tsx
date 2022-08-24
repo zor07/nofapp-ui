e525fb162819109e35cb63b0bedf719dc601efab
zor07 <anzor.karmov@gmail.com>
2022-08-24 17:45:38 +0300

    user list draft

--- src/components/Profile/list/ProfileListContainer.tsx
+++ src/components/Profile/list/ProfileListContainer.tsx
@@ -1,10 +1,53 @@
-import React from "react"
+import React, {useEffect} from "react"
+import {ProfileType} from "../../../redux/profile-reducer";
+import {withAuthRedirect} from "../../../hoc/withAuthRedirect";
+import {AppStateType} from "../../../redux/redux-store";
+import {compose} from "redux";
+import {connect, useDispatch} from "react-redux";
+import {getProfiles} from "../../../redux/profile-list-reducer";
 
-const Profile = () => {
+
+type MapStatePropsType = {
+    profiles: Array<ProfileType>
+}
+
+type MapDispatchPropsType = {
+    getProfiles: () => void
+}
+
+type ProfileListContainerPropsType = MapStatePropsType & MapDispatchPropsType
+
+const ProfileListContainer: React.FC<ProfileListContainerPropsType> = ({profiles}) => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getProfiles())
+    }, [])
+
+    const profileElements = profiles.map(p => (
+        <div>
+            <div>{p.id}</div>
+            <div>{p.user.name}</div>
+            <div><img src={p.avatarUri}/> </div>
+            <div>{p.timerStart.toDateString()}</div>
+        </div>
+    ))
+
     return (
-        <div>Profile List</div>
+        <div>{profileElements}</div>
     )
 }
 
+let mapStateToProps = (state: AppStateType): MapStatePropsType => {
+    return {
+        profiles: state.profileList.profiles
+    }
+}
 
-export default Profile
+export default compose(
+    withAuthRedirect,
+    connect<MapStatePropsType, MapDispatchPropsType, AppStateType>(mapStateToProps, {
+        getProfiles
+    })
+)(ProfileListContainer);
